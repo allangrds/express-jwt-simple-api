6d746aadbd43cd3e7a180e5929ff3391b59e3164
Allan Ramos <allangrds@gmail.com>
2019-06-19 21:39:07 -0300

    wip 10

--- app/http/controllers/users.js
+++ app/http/controllers/users.js
@@ -1,10 +1,8 @@
-// const { User } = require('../../models')
-// const paginate = require('../../models/helpers')
 const User = require('../repositories/User')
 
 class Users {
   async index (req, res) {
-    const { page = 1 } = req.params
+    const { page } = req.query
     const users = await User.getAll(page)
 
     res.status(200).send({
--- app/http/repositories/Base.js
+++ app/http/repositories/Base.js
@@ -2,18 +2,19 @@ const paginate = require('../../models/helpers')
 
 const BaseRepository = {
   getAll: async (model, page, attributes) => {
+    const reqPage = page || 1
     const results = await model.findAll(paginate(
       {
         attributes,
       },
       {
-        page: page || 1,
+        page: reqPage,
       }
     ))
-    const total = await model.count()
+    const total = results.length ? await model.count() : 0
 
     return {
-      current_page: results.length ? page : 0,
+      current_page: results.length ? reqPage : 0,
       per_page: results.length || 0,
       results,
       total,
